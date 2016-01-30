189e473c6d4313d8882fdde1ab21ad5059bebd4f
bcr4504 <bcrprogramming@gmail.com>
2016-01-29 20:05:05 -0500

    modified:   BCR 4504/Debug/FRCUserProgram 	modified:   BCR 4504/Debug/src/Robot.o 	modified:   BCR 4504/src/Robot.cpp 	modified:   BCR 4504/sysProps.xml

--- BCR 4504/src/Robot.cpp
+++ BCR 4504/src/Robot.cpp
@@ -12,38 +12,44 @@ class Robot: public SampleRobot
 	RobotDrive *myRobot = new RobotDrive(left, right);
 	SendableChooser *chooser = new SendableChooser();
 	const std::string autoNameDefault = "Default";
-	const std::string autoNameCustom = "Stronghold";
-	Gyro gyro;
-	static const double Kp = 0.03;
+	AnalogInput *tiltsensor = new AnalogInput(0);
+	AnalogGyro *gyro = new AnalogGyro(1);
+	
 	void Autonomous()
 	{
-		gyro.Reset (0);
-		while (IsAutonomous(0))
-		{
-			GetWatchdog().Feed();
-			double angle = gyro.GetAngle(0);
-			myRobot.Drive(-1.0, -angle * Kp);
-			Wait(0.004);
-		}
+		static const double Kp = 0.03;
+		gyro->Reset();
 		//std::string autoSelected = *((std::string*)chooser->GetSelected());
 		std::string autoSelected = SmartDashboard::GetString("Auto Selector", autoNameDefault);
 		myRobot->SetSafetyEnabled(false);
-		myRobot->SetLeftRightMotorOutputs(0.5, 0.0);
+		bool flag;
+		while(!flag)
+		{
+			double angle = gyro->GetAngle();
+			myRobot->Drive(0.25, -angle * Kp);
+			static int revolutions = 0;
+			Wait(0.004);
+			revolutions++;
+			if(revolutions == 2500)
+			{
+				flag = true;
+			}
+		}
 		//Add condition to stop when the ballast comes into view. Need to wait until vision can be done.
-		Wait(10.0);
 		myRobot->SetLeftRightMotorOutputs(0.0, 0.0);
 	}
-
-
 
 	void RobotInit()
 	{
 		myRobot->SetExpiration(0.1);
+		gyro->Calibrate();
+		gyro->SetSensitivity(0.007);
 	}
 
 	void OperatorControl()
 	{
 		myRobot->SetSafetyEnabled(false);
+		gyro->Reset();
 		//motor1->EnableControl();
 		while (IsOperatorControl() and IsEnabled())
 		{
@@ -62,6 +68,8 @@ class Robot: public SampleRobot
 				t_drive = false;
 				a_drive = true;
 			}
+			SmartDashboard::PutNumber("Gyro Rate: ", gyro->GetRate());
+			SmartDashboard::PutNumber("Gyro Angle: ", gyro->GetAngle());
 			if(stick->GetRawButton(5))
 			{
 				if(stick->GetRawAxis(2) < 0.1)
