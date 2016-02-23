a25761fab6a2b23b5346d223bd2d411e022fa0d9
bcr4504 <bcrprogramming@gmail.com>
2016-02-23 16:59:59 -0500

    modified:   BCR 4504/src/Robot.cpp

--- BCR 4504/src/Robot.cpp
+++ BCR 4504/src/Robot.cpp
@@ -4,14 +4,14 @@
 class Robot: public SampleRobot
 {
 	//typedef void (*funcp)(bool on);
-	CANTalon *right = new CANTalon(1);
-	CANTalon *left = new CANTalon(0);
-	DoubleSolenoid *arm = new DoubleSolenoid(6,7);
-	Solenoid *gripper = new Solenoid(1);
-	Solenoid *gripper2 = new Solenoid(4);
-	Solenoid *shooter = new Solenoid(5);
+	CANTalon *right = new CANTalon(0);
+	CANTalon *left = new CANTalon(1);
+	DoubleSolenoid *arm = new DoubleSolenoid(3,7,6);
+	Solenoid *gripper = new Solenoid(3,1);
+	Solenoid *gripper2 = new Solenoid(3,4);
+	Solenoid *shooter = new Solenoid(3,5);
 	//Solenoid *gearshift = new Solenoid(7);
-	Compressor *compressor = new Compressor(0);
+	Compressor *compressor = new Compressor(3);
 	Joystick *stick = new Joystick(0);
 	Joystick *buttons = new Joystick(1);
 	RobotDrive *myRobot = new RobotDrive(left, right/*, fr, bl*/);
@@ -21,25 +21,15 @@ class Robot: public SampleRobot
 	AnalogGyro *gyro = new AnalogGyro(1);
 	std::vector<double> gyros;
 	BuiltInAccelerometer *accel = new BuiltInAccelerometer(Accelerometer::Range::kRange_8G);
-	const double kUpdatePeriod = 0.005;
+	//const double kUpdatePeriod = 0.005;
 	const double Kp = 0.003;
 	const double Ki = 0.003;
-	CameraServer camserver;
-	AxisCamera *camera;
-	Image *camimage;
+	//CameraServer camserver;
+	//AxisCamera *camera;
+	//Image *camimage;
+	enum drive_angles {DO_NOTHING = -1, PORT_ANGLE = 20, DRIVE_ANGLE = 25, SHOOT_ANGLE = 50};
 	int error = 0;
 public:
-	/*void toggle(funcp, bool joy, bool curr, bool &flag)
-	{
-		if(joy && !flag)
-		{
-			funcp(!curr);
-			flag = true;
-		}else if(!joy && flag)
-		{
-			flag = false;
-		}
-	}*/
 	void Autonomous() override
 	{
 		gyro->Reset();
@@ -65,55 +55,54 @@ public:
 		myRobot->SetExpiration(1);
 		gyro->SetSensitivity(0.007);
 		gyro->Calibrate();
-		camera = new AxisCamera("axis-camera.local");
-		camimage = imaqCreateImage(IMAQ_IMAGE_RGB,0);
+		//camera = new AxisCamera("axis-camera.local");
+		//camimage = imaqCreateImage(IMAQ_IMAGE_RGB,0);
 	}
 
 	void OperatorControl()
 	{
 		myRobot->SetSafetyEnabled(false);
 		gyro->Reset();
-		//motor1->EnableControl();
 		while (IsOperatorControl() and IsEnabled())
 		{
-			camera->GetImage(camimage);
-			camserver.SetImage(camimage);
-			double tiltvalue = (round(abs(tiltsensor->GetValue())/10)*10)*(9.0/197.0);
-			SmartDashboard::PutNumber("Tilt Sensor: ", tiltvalue);
+			//camera->GetImage(camimage);
+			//camserver.SetImage(camimage);
+			double angle = (round(abs(tiltsensor->GetValue())/10)*10)*(9.0/197.0);
+			double epsilon = 0.1;
+			static double goalangle = DO_NOTHING;
+			if(stick->GetRawButton(1))
+				goalangle = DRIVE_ANGLE;
+			if(stick->GetRawButton(2))
+				goalangle = DO_NOTHING;
+			if(stick->GetRawButton(3))
+				goalangle = PORT_ANGLE;
+			if(stick->GetRawButton(4))
+				goalangle = SHOOT_ANGLE;
+
+			SmartDashboard::PutNumber("Tilt Sensor: ", angle);
 			SmartDashboard::PutNumber("Motor Y: ", stick->GetY());
 			SmartDashboard::PutNumber("Motor X: ", stick->GetX());
-			bool t_drive = false;
-			bool a_drive = true;
+			bool t_drive = true;
+			bool a_drive = false;
 			static bool flag = true;
-			//static bool flag2 = true;
-			//static bool flag3 = true;
-			//static bool flag4 = true;
+			static bool flag2 = true;
+			static bool flag3 = true;
+			static bool flag4 = true;
+			static bool flag5 = true;
 			SmartDashboard::PutBoolean("Compressor ",compressor->Enabled());
-			//static bool toggle = true;
-			//void toggle(funcp, bool joy, bool curr, bool &flag)
-			//toggle(compressor->SetCompressor,stick->GetRawButton(1),compressor->Enabled(),flag);
-			if(stick->GetRawButton(1) && !flag)
+			if(buttons->GetRawButton(5) && !flag)
 			{
-				//compressor->SetCompressor(!compressor->Enabled());
 				compressor->Enabled() ? compressor->Stop() : compressor->Start();
 				flag = true;
-			}else if(!stick->GetRawButton(1) && flag)
+			}else if(!buttons->GetRawButton(5) && flag)
 			{
 				flag = false;
 			}
-			//Porculus arm lifting toggle?
-
-			/*if(buttons->GetRawButton(2) && !flag2)
-			{
-				armup->Set(true);
-				armdown->Set(false);
-				flag2 = true;
-			}else if(!buttons->GetRawButton(2) && flag2)
-			{
-				armup->Set(false);
-				armdown->Set(true);
-				flag2 = false;
-			}*/
+			/*2 settings for tilt sensor. Find angles
+			 * Driving 1
+			 * Portcullis (auto) 3
+			 * Low Goal 4
+			 */
 			//Gear Shift toggle
 			//toggle(gearshift->Set,buttons->GetRawButton(3),gearshift->Get(),flag3);
 			/*if(buttons->GetRawButton(3) && !flag3)
@@ -125,51 +114,74 @@ public:
 				gearshift->Set(false);
 				flag3 = false;
 			}*/
-			//Gripper on?
-			//toggle(gripper->Set,stick->GetRawButton(2),gripper->Get(),flag4);
-			if(stick->GetRawButton(2))
+			//Gripper and Clamp
+			if(buttons->GetRawButton(10) && !flag2)
 			{
-				gripper->Set(true);
-				Wait(0.5); // test times
-				gripper2->Set(true);
+				//compressor->SetCompressor(!compressor->Enabled());
+				gripper->Set(!gripper->Get());
+				Wait(0.5);
+				gripper2->Set(!gripper2->Get());
+				flag2 = true;
+			}else if(!buttons->GetRawButton(10) && flag2)
+			{
+				flag2 = false;
 			}
-			//Degrip?
-			if(stick->GetRawButton(10))
+			//Clamp
+			if(buttons->GetRawButton(1) && !flag3)
 			{
-				gripper2->Set(false);
-				Wait(0.1);
-				gripper->Set(false);
+				gripper2->Set(!gripper2->Get());
+				flag3 = true;
+			}else if(!buttons->GetRawButton(1) && flag3)
+			{
+				flag3 = false;
 			}
-			//Shoot?
-			if(stick->GetRawButton(4))
+			//Gripper
+			if(buttons->GetRawButton(2) && !flag4)
 			{
+				gripper->Set(!gripper->Get());
+				flag4 = true;
+			}else if(!buttons->GetRawButton(2) && flag4)
+			{
+				flag4 = false;
+			}
+			//Shoot
+			if(buttons->GetRawButton(4) && !flag5)
+			{
+				if(gripper2->Get())
+					gripper2->Set(false);
 				shooter->Set(true);
-				Wait(0.05);
+				Wait(.25);
 				shooter->Set(false);
+				flag5 = true;
+			}else if(!buttons->GetRawButton(4) && flag5)
+			{
+				flag5 = false;
 			}
-			if(stick->GetPOV(0) == 90)
+			//Arm Up
+			if(buttons->GetRawButton(8))
 			{
 				arm->Set(DoubleSolenoid::kForward);
-			}else if(stick->GetPOV(0) == 270)
+			//Arm Down
+			}else if(buttons->GetRawButton(11))
 			{
 				arm->Set(DoubleSolenoid::kReverse);
 			}else
 			{
 				arm->Set(DoubleSolenoid::kOff);
 			}
+
+			//Tank Drive switch
 			if(buttons->GetRawButton(7))
-			{
-				SmartDashboard::PutString("Drive Mode: ", "Tank Drive");
-				t_drive = true;
-				a_drive = false;
-			}else
 			{
 				SmartDashboard::PutString("Drive Mode: ", "Arcade Drive");
 				t_drive = false;
 				a_drive = true;
+			}else
+			{
+				SmartDashboard::PutString("Drive Mode: ", "Tank Drive");
+				t_drive = true;
+				a_drive = false;
 			}
-
-			//SmartDashboard::PutNumber("Gyro Rate: ", gyro->GetRate());
 			SmartDashboard::PutNumber("Gyro Angle: ", fmod(gyro->GetAngle(),360.0));
 			//SmartDashboard::PutNumber("Distance to Image:, ", computeDistance(binimage, ));
 			if(stick->GetRawButton(5))
@@ -188,19 +200,12 @@ public:
 				{
 					myRobot->ArcadeDrive(-stick->GetRawAxis(1), -stick->GetRawAxis(0), true);
 				}
-				/*if(buttons->GetRawButton(3))
-				{
-					motor1->SetPID(1, 3, 4);
-					motor1->Set(f);
-				}else if(buttons->GetRawButton(4))
-				{
-					motor1->SetPID(1, 3, 4);
-					motor1->Set(-f);
-				}*/
+				//PID with shooter
 			}else
 			{
 				myRobot->SetLeftRightMotorOutputs(0.0, 0.0);
 			}
+			/*
 			double previousX = 0;
 			double previousY = 0;
 			double previousZ = 0;
@@ -221,7 +226,8 @@ public:
 			previousX = (xAcceleration*0.1) + (0.9*previousX);
 			previousY = (yAcceleration*0.1) + (0.9*previousY);
 			previousZ = (zAcceleration*0.1) + (0.9*previousZ);
-			Wait(kUpdatePeriod);
+			*/
+			Wait(0.005);
 		}
 	}
 
